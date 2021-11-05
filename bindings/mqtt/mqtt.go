7abd3ad17138adc43dbb1adf1d9247c76b9f6830
Simon Leet <simon.leet@microsoft.com>
2021-11-05 19:56:13 +0000

    TEST: Mock MQTT bindings Read() to isolate Invoke() client trace

--- bindings/mqtt/mqtt.go
+++ bindings/mqtt/mqtt.go
@@ -14,9 +14,7 @@ import (
 	"log"
 	"net/url"
 	"os"
-	"os/signal"
 	"strconv"
-	"syscall"
 	"time"
 
 	"github.com/cenkalti/backoff/v4"
@@ -24,7 +22,6 @@ import (
 
 	"github.com/dapr/components-contrib/bindings"
 	"github.com/dapr/kit/logger"
-	"github.com/dapr/kit/retry"
 )
 
 const (
@@ -203,77 +200,93 @@ func (m *MQTT) Invoke(req *bindings.InvokeRequest) (*bindings.InvokeResponse, er
 	return nil, nil
 }
 
-func (m *MQTT) handleMessage(handler func(*bindings.ReadResponse) ([]byte, error), mqttMsg mqtt.Message) error {
-	msg := bindings.ReadResponse{Data: mqttMsg.Payload()}
+// func (m *MQTT) handleMessage(handler func(*bindings.ReadResponse) ([]byte, error), mqttMsg mqtt.Message) error {
+// 	msg := bindings.ReadResponse{Data: mqttMsg.Payload()}
 
-	// paho.mqtt.golang requires that handlers never block or it can deadlock on client.Disconnect.
-	// To ensure that the Dapr runtime does not hang on teardown on of the component, run the app's
-	// handling code in a goroutine so that this handler function is always cancellable on Close().
-	ch := make(chan error)
-	go func(m *bindings.ReadResponse) {
-		defer close(ch)
-		_, err := handler(m)
-		ch <- err
-	}(&msg)
+// 	// paho.mqtt.golang requires that handlers never block or it can deadlock on client.Disconnect.
+// 	// To ensure that the Dapr runtime does not hang on teardown on of the component, run the app's
+// 	// handling code in a goroutine so that this handler function is always cancellable on Close().
+// 	ch := make(chan error)
+// 	go func(m *bindings.ReadResponse) {
+// 		defer close(ch)
+// 		_, err := handler(m)
+// 		ch <- err
+// 	}(&msg)
 
-	select {
-	case handlerErr := <-ch:
-		if handlerErr != nil {
-			return handlerErr
-		}
-		mqttMsg.Ack()
-		return nil
-	case <-m.ctx.Done():
-		m.logger.Infof("Read context cancelled: %v", m.ctx.Err())
-		return m.ctx.Err()
-	}
-}
+// 	select {
+// 	case handlerErr := <-ch:
+// 		if handlerErr != nil {
+// 			return handlerErr
+// 		}
+// 		mqttMsg.Ack()
+// 		return nil
+// 	case <-m.ctx.Done():
+// 		m.logger.Infof("Read context cancelled: %v", m.ctx.Err())
+// 		return m.ctx.Err()
+// 	}
+// }
 
+// func (m *MQTT) Read(handler func(*bindings.ReadResponse) ([]byte, error)) error {
+// 	sigterm := make(chan os.Signal, 1)
+// 	signal.Notify(sigterm, os.Interrupt, syscall.SIGTERM)
+
+// 	// reset synchronization
+// 	if m.consumer != nil {
+// 		m.logger.Warnf("re-initializing the subscriber")
+// 		m.consumer.Disconnect(0)
+// 		m.consumer = nil
+// 	}
+
+// 	// mqtt broker allows only one connection at a given time from a clientID.
+// 	consumerClientID := fmt.Sprintf("%s-consumer", m.metadata.clientID)
+// 	c, err := m.connect(consumerClientID)
+// 	if err != nil {
+// 		return err
+// 	}
+// 	m.consumer = c
+
+// 	m.logger.Debugf("mqtt subscribing to topic %s", m.metadata.topic)
+// 	token := m.consumer.Subscribe(m.metadata.topic, m.metadata.qos, func(client mqtt.Client, mqttMsg mqtt.Message) {
+// 		b := m.backOff
+// 		if m.metadata.backOffMaxRetries >= 0 {
+// 			b = backoff.WithMaxRetries(m.backOff, uint64(m.metadata.backOffMaxRetries))
+// 		}
+
+// 		if err := retry.NotifyRecover(func() error {
+// 			m.logger.Debugf("Processing MQTT message %s/%d", mqttMsg.Topic(), mqttMsg.MessageID())
+// 			return m.handleMessage(handler, mqttMsg)
+// 		}, b, func(err error, d time.Duration) {
+// 			m.logger.Errorf("Error processing MQTT message: %s/%d. Retrying...", mqttMsg.Topic(), mqttMsg.MessageID())
+// 		}, func() {
+// 			m.logger.Infof("Successfully processed MQTT message after it previously failed: %s/%d", mqttMsg.Topic(), mqttMsg.MessageID())
+// 		}); err != nil {
+// 			m.logger.Errorf("Failed processing MQTT message: %s/%d: %v", mqttMsg.Topic(), mqttMsg.MessageID(), err)
+// 		}
+// 	})
+// 	if err := token.Error(); err != nil {
+// 		m.logger.Errorf("mqtt error from subscribe: %v", err)
+
+// 		return err
+// 	}
+// 	<-sigterm
+
+// 	return nil
+// }
+
+//TEST: Mock Read to avoid interactions with mqtt publish client
 func (m *MQTT) Read(handler func(*bindings.ReadResponse) ([]byte, error)) error {
-	sigterm := make(chan os.Signal, 1)
-	signal.Notify(sigterm, os.Interrupt, syscall.SIGTERM)
-
-	// reset synchronization
-	if m.consumer != nil {
-		m.logger.Warnf("re-initializing the subscriber")
-		m.consumer.Disconnect(0)
-		m.consumer = nil
-	}
-
-	// mqtt broker allows only one connection at a given time from a clientID.
-	consumerClientID := fmt.Sprintf("%s-consumer", m.metadata.clientID)
-	c, err := m.connect(consumerClientID)
-	if err != nil {
-		return err
-	}
-	m.consumer = c
-
-	m.logger.Debugf("mqtt subscribing to topic %s", m.metadata.topic)
-	token := m.consumer.Subscribe(m.metadata.topic, m.metadata.qos, func(client mqtt.Client, mqttMsg mqtt.Message) {
-		b := m.backOff
-		if m.metadata.backOffMaxRetries >= 0 {
-			b = backoff.WithMaxRetries(m.backOff, uint64(m.metadata.backOffMaxRetries))
+	m.logger.Debugf("MOCK: mqtt subscribing to topic %s", m.metadata.topic)
+	interval := time.Second
+	for {
+		timer := time.After(interval)
+		<-timer
+		m.logger.Debug("MOCK: invoke Read handler...")
+		msg := bindings.ReadResponse{}
+		if _, err := handler(&msg); err != nil {
+			m.logger.Errorf("MOCK: Read is never expected to terminate with err from handler %s", err)
+			return err
 		}
-
-		if err := retry.NotifyRecover(func() error {
-			m.logger.Debugf("Processing MQTT message %s/%d", mqttMsg.Topic(), mqttMsg.MessageID())
-			return m.handleMessage(handler, mqttMsg)
-		}, b, func(err error, d time.Duration) {
-			m.logger.Errorf("Error processing MQTT message: %s/%d. Retrying...", mqttMsg.Topic(), mqttMsg.MessageID())
-		}, func() {
-			m.logger.Infof("Successfully processed MQTT message after it previously failed: %s/%d", mqttMsg.Topic(), mqttMsg.MessageID())
-		}); err != nil {
-			m.logger.Errorf("Failed processing MQTT message: %s/%d: %v", mqttMsg.Topic(), mqttMsg.MessageID(), err)
-		}
-	})
-	if err := token.Error(); err != nil {
-		m.logger.Errorf("mqtt error from subscribe: %v", err)
-
-		return err
 	}
-	<-sigterm
-
-	return nil
 }
 
 func (m *MQTT) connect(clientID string) (mqtt.Client, error) {
